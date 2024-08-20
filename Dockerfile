3f3ae728f7297227e12da573bd5bcf557347ae37
befragment <q.levchenko@gmail.com>
2024-08-20 15:21:47 +0300

    added some dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-FROM python:3.10.4-slim
+FROM python:3.10.4
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 COPY . .
-
 
 CMD [ "python3", "main.py" ]
--- main.py
+++ main.py
@@ -64,6 +64,6 @@ if __name__ == "__main__":
         app='main:app', 
         host="127.0.0.1", 
         port=8001, 
-        reload=True, 
+        reload=False, 
         access_log=False
     )
